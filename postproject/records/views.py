dea9e0292c53e3335b0e6acb89643e852beb3271
ptierni <ptierni@gmail.com>
2017-08-14 19:33:37 -0400

    first commit

---
+++ postproject/records/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from records.models import Post
+
+class PostListView(ListView):
+
+	model = Post
+
+class PostDetailView(DetailView):
+
+	model = Post
+
+	
